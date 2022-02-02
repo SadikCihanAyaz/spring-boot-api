1af5fcd31d23ad9e6f4f6d333a1697a1bbf80c1e
cihan ayaz <scayaz.19.19@gmail.com>
2022-02-02 16:04:45 +0300

    feature: theme park ride getride repository added

---
+++ src/main/java/com/cihan/repository/ThemeParkRideRepository.java
@@ -0,0 +1,10 @@
+package com.cihan.repository;
+
+import com.cihan.entity.ThemeParkRide;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+@Repository
+public interface ThemeParkRideRepository extends CrudRepository<ThemeParkRide, Long> {
+    List<ThemeParkRide> findByName(String name);
+}
